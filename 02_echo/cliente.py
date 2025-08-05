6671c9821264afd9493fb5edf6c3a23e4a9fdc00
lorenqr <lorenagr223@gmail.com>
2025-08-05 18:51:13 -0500

    Información cliente - servidor Echo

--- 02_echo/cliente.py
+++ 02_echo/cliente.py
@@ -1,2 +1,19 @@
 # Cliente
 
+import socket
+
+HOST = 'localhost'
+PORT = 9000
+
+mensaje = input("Digite tu mensaje: ")
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+cliente.connect((HOST, PORT))
+
+
+cliente.sendall(mensaje)
+print(f"Mensaje enviados: '{mensaje}'")
+
+respuesta = cliente.recv(1024)
+print(f"Respuesta del 'Echo': '{respuesta}'")
+
+cliente.close()
