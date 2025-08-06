ef69d1df58676eb76f0755ad295a1004458c4ee1
lorenqr <lorenagr223@gmail.com>
2025-08-05 22:44:34 -0500

    Creacion de la conexion con el cliente - para vista de un html

--- 04_http/cliente.py
+++ 04_http/cliente.py
@@ -1,2 +1,14 @@
 # Cliente
+import socket
+import http.client
 
+HOST = 'localhost'
+PORT = 9000
+
+cliente = http.client.HTTPConnection(HOST, PORT)
+cliente.request('GET', '/')
+
+respuesta = cliente.getresponse()
+datos = respuesta.read().decode()
+
+print(datos)
