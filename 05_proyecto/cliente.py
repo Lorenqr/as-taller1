1dfa0d3b9f6b72f1bb3e8ca8eb02a04675e21995
lorenqr <lorenagr223@gmail.com>
2025-08-08 19:56:58 -0500

    Se crea el cliente el cual se conecta con el servidor y le manda una ecuacion y el servidor le arroja la respuesta correcta del servidor para terminar de ejcutar ecuaciones escribe salir

--- 05_proyecto/cliente.py
+++ 05_proyecto/cliente.py
@@ -7,7 +7,7 @@ PORT = 9000
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
 
-print("Conectado al servidor de cálculo. Escribe 'salir' para terminar.")
+print("conectado al servidor de cálculo. Escribe 'salir' para terminar.")
 
 while True:
     operacion = input("Ingresa operación: ")
